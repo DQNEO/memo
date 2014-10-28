ca6144b499f7fd6e5ce49ada94c6a82e04493c60
DQNEO <dqneoo@gmail.com>
2014-10-28 23:12:35 +0900

    Config::Tinyを使って~/.aws/configの値を取得する

---
+++ perl/use_config_tiny.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+use v5.18;
+use warnings;
+use Config::Tiny;
+use Data::Dumper;
+
+my $file = $ENV{'HOME'} . '/.aws/config';
+my $cfg = new Config::Tiny->read($file) or die "cannot parse config";
+
+# 全体
+print Dumper $cfg;
+
+# ブロック
+print Dumper $cfg->{default};
