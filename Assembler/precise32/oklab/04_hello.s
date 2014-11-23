d9d04dd878ca9d81ad428ce69eee25d5a9a7dee9
DQNEO <dqneoo@gmail.com>
2014-11-23 17:06:43 +0900

    文字列長を自動で計算してやる。

---
+++ Assembler/precise32/oklab/04_hello.s
@@ -0,0 +1,27 @@
+        /*
+         * GASでのHelloWorldプログラム
+         */
+        .code32
+        .text
+        .globl main
+main:
+
+          /*
+           * mov命令によりデータをレジスタに設定する。
+           * movl 入力先 出力先
+           */
+          movl $0x4, %eax         # AXレジスタに数値4を設定
+          movl $0x1, %ebx         # BXレジスタに数値1を設定
+          movl $msg,%ecx        # CXレジスタにmsgラベルを設定
+          movl $msglen, %edx         # DXレジスタに数値5を指定
+
+          # システムコールの実行
+          int  $0x80
+
+          ret
+
+        .data
+
+          # データはこちらに書いていく。
+msg:     .ascii "hello\n"
+msgend:  msglen = msgend - msg
