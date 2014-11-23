01a97c437c52e546bf03fe95b476aa8d940366f0
DQNEO <dqneoo@gmail.com>
2014-11-23 16:59:57 +0900

    数値を16進数で書く。

--- Assembler/precise32/oklab/03_hello.s
+++ Assembler/precise32/oklab/03_hello.s
@@ -10,10 +10,10 @@ main:
            * mov命令によりデータをレジスタに設定する。
            * movl 入力先 出力先
            */
-          movl $4, %eax         # AXレジスタに数値4を設定
-          movl $1, %ebx         # BXレジスタに数値1を設定
+          movl $0x4, %eax         # AXレジスタに数値4を設定
+          movl $0x1, %ebx         # BXレジスタに数値1を設定
           movl $msg,%ecx        # CXレジスタにmsgラベルを設定
-          movl $6, %edx         # DXレジスタに数値5を指定
+          movl $0x6, %edx         # DXレジスタに数値5を指定
 
           # システムコールの実行
           int  $0x80
