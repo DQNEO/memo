084d5a6f18001042cd32e31c88b09a3da2c87d87
DQNEO <dqneoo@gmail.com>
2014-11-25 00:49:11 +0900

    moved

--- Assembler/precise32/fizbuz/fiz.s
+++ Assembler/precise32/fizbuz/fiz.s
@@ -8,9 +8,11 @@ fizend:  .equ  lenfiz, fizend - fiz
         //ここからスタート
         .global main
 main:
+        # fiz
         movl $1,        %ebx
         movl $fiz,      %ecx
         movl $lenfiz,      %edx
         movl $4,        %eax    # write system call(sys_write)
         int  $0x80
-        ret
+
+ret
