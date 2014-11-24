02d64ed37f8671008077a6144918e254c40a3b81
DQNEO <dqneoo@gmail.com>
2014-11-25 02:15:50 +0900

    printfを使わないwrite(2)だけのhello worldができた！！

--- Assembler/precise32/fizbuz/fiz.s
+++ Assembler/precise32/fizbuz/fiz.s
@@ -24,4 +24,21 @@ main:
         movl $4,        %eax    # write system call(sys_write)
         int  $0x80
 
+        pushl  %ebp
+        movl %esp, %ebp
+        movb $0x00, -1(%ebp) # \0
+        movb $0x0A, -2(%ebp) # \n
+        movb $0x6f, -3(%ebp) # o
+        movb $0x6c, -4(%ebp) # l
+        movb $0x6c, -5(%ebp) # l
+        movb $0x65, -6(%ebp) # e
+        movb $0x68, -7(%ebp) # h
+
+        movl $4,        %eax   # sys_write
+        movl $1,        %ebx   # stdout
+        leal -7(%ebp),  %ecx   # address of 'h'
+        movl $7,        %edx   # len 7
+        int  $0x80
+
+        popl  %ebx  # おまじない
         ret
