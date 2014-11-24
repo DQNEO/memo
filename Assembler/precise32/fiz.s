6b7a7a75e932e489410219014db83cb396a32e33
DQNEO <dqneoo@gmail.com>
2014-11-25 00:20:14 +0900

    fizするだけのミニマムなコード

---
+++ Assembler/precise32/fiz.s
@@ -0,0 +1,16 @@
+.file   "fiz.s"
+        .data
+#文字列を定義
+fiz:   .ascii  "fiz\n"
+#文字列長を計算
+fizend:  .equ  lenfiz, fizend - fiz
+
+        //ここからスタート
+        .global main
+main:
+        movl $1,        %ebx
+        movl $fiz,      %ecx
+        movl $lenfiz,      %edx
+        movl $4,        %eax    # write system call(sys_write)
+        int  $0x80
+        ret
