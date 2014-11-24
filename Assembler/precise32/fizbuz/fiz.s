5764f4c5a52a548efeac14db49ee0e12be84b2f3
DQNEO <dqneoo@gmail.com>
2014-11-25 00:50:09 +0900

    add baz

--- Assembler/precise32/fizbuz/fiz.s
+++ Assembler/precise32/fizbuz/fiz.s
@@ -5,6 +5,8 @@ fiz:   .ascii  "fiz\n"
 #文字列長を計算
 fizend:  .equ  lenfiz, fizend - fiz
 
+baz:   .ascii  "baz\n"
+bazend:  .equ  lenbaz, bazend - baz
         //ここからスタート
         .global main
 main:
@@ -15,4 +17,10 @@ main:
         movl $4,        %eax    # write system call(sys_write)
         int  $0x80
 
+        # baz
+        movl $1,        %ebx
+        movl $baz,      %ecx
+        movl $lenbaz,   %edx
+        movl $4,        %eax    # write system call(sys_write)
+        int  $0x80
 ret
